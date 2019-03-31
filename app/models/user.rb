569e5c181b200610de44e8bf6c09d099c17f7fcd
KazuhiroObama <obamakazuhiro@gmail.com>
2019-03-31 22:28:14 +0900

    テスト実装、リサイズして表示
    表示時ではなく保存時にリサイズしたい。edit画面で画像が削除されないようにしたい

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,24 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { minimum: 4 }
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, length: { minimum: 8 } # has_secure_passwordメソッドにより、presence: trueも含まれる
+  validate :validate_image
+
+  def icon
+    return self.image.variant(resize: '200x200')
+  end
+
+  private
+
+  def validate_image
+    return unless image.attached?
+    
+    errors.add(:image, I18n.t('errors.messages.too_large')) if image.blob.byte_size > 5.megabytes
+    errors.add(:image, I18n.t('errors.messages.not_jpg_or_png')) unless jpg_or_png?
+    image.purge if self.errors.messages[:image].present?
+  end
+
+  def jpg_or_png?
+    permission_file_format = %w[image/jpeg image/png]
+    permission_file_format.include?(image.blob.content_type)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'Userが新規登録される' do
-    expect(FactoryBot.create(:user)).to be_valid
+  let(:current_user) { FactoryBot.build(:user) }
+
+  describe '画像アップロード' do
+    context '画像のファイル形式がgifの時' do
+      image_file = Rack::Test::UploadedFile.new('spec/factories/hamster_01.gif', 'image/gif')
+      let(:gif_image) { FactoryBot.build(:user, image: image_file ) }
+
+      it 'jpegかpngでアップロードしてください' do
+        expect(gif_image).not_to be_valid
+      end
+    end
+
+    context '画像のファイル形式が正常の時' do
+      image_file = Rack::Test::UploadedFile.new('spec/factories/cat_1.png', 'image/png')
+      let(:valid_image) { FactoryBot.build(:user, image: image_file ) }
+
+      it 'jpegかpngでアップロードしてください' do
+        expect(valid_image).to be_valid
+      end
+    end
   end
 end
